0b2c03d51c983c4cd93b8e0b238a331af97db832
vikesh jha <vikeshjha39@gmail.com>
2025-07-24 14:14:01 +0530

    link

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 3000;
 connectDB();
 
 app.use(cors({
-  origin: 'https://test-uj6e.onrender.com',
+  origin: 'https://collaborative-whiteboard-5v5u.onrender.com',
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization', 'Accept']
